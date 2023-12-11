ae2c8e9c2b87820b751fd2f34c42c3575ec65c7b
Damon <126731021+damon314159@users.noreply.github.com>
2023-12-11 16:02:56 +0000

    Day 11 solutions

---
+++ solutions/11/day11-1.js
@@ -0,0 +1,50 @@
+const fs = require('fs')
+const path = require('path')
+
+const inputPath = path.join(__dirname, 'data.txt')
+const readFile = (p) => fs.readFileSync(p, { encoding: 'utf8' })
+// Input as array of lines
+const input = readFile(inputPath).split('\n')
+
+// Solving time!
+const result = ((stretched) =>
+  stretched[0]
+    .split('')
+    .map((_, i) => stretched.map((line) => line[i]).join(''))
+    .map((line) => (line.includes('#') ? line : 'M'.repeat(line.length))))(
+  input.map((line) => (line.includes('#') ? line : 'M'.repeat(line.length)))
+)
+  .reduce(
+    ([x, y], line, r, expanded) =>
+      (([lineX, lineY]) => [x.concat(lineX), y.concat(lineY)])(
+        line.split('').reduce(
+          ([lineX, lineY], char, c) =>
+            char === '#'
+              ? [
+                  lineX.concat(
+                    ((subLine) => subLine.replace(/[^M]/g, '').length * 2 + subLine.replace(/M/g, '').length)(
+                      expanded
+                        .map((l) => l[c])
+                        .slice(0, r)
+                        .join('')
+                    )
+                  ),
+                  lineY.concat(
+                    ((subLine) => subLine.replace(/[^M]/g, '').length * 2 + subLine.replace(/M/g, '').length)(
+                      line.slice(0, c)
+                    )
+                  )
+                ]
+              : [lineX, lineY],
+          [[], []]
+        )
+      ),
+    [[], []]
+  )
+  .reduce(
+    (sum, coord) =>
+      sum + coord.sort((a, b) => a - b).reduce(([p, t], next, i) => [p + next * i - t, t + next], [0, 0])[0],
+    0
+  )
+
+console.log(result)
