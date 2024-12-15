e511df06039bf261610ad469aacda6e0b45f3ba3
Damon <126731021+damon314159@users.noreply.github.com>
2024-12-15 17:23:42 +0000

    Day 15 solutions

---
+++ 2024/15/part1.js
@@ -0,0 +1,84 @@
+const fs = require("fs");
+const path = require("path");
+
+// Read data
+const inputPath = path.join(__dirname, "data.txt");
+const readFile = (p) => fs.readFileSync(p, { encoding: "utf8" });
+const input = readFile(inputPath).trim();
+
+// Solution
+// Input parsing
+const [map, instructions] = input.split("\n\n");
+const grid = map.split("\n").map((line) => line.split(""));
+const moves = instructions.replace(/\n/g, "").split("");
+
+// Grid helpers
+const directions = {
+  "^": { row: -1, col: 0 }, // Up
+  ">": { row: 0, col: 1 }, // Right
+  v: { row: 1, col: 0 }, // Down
+  "<": { row: 0, col: -1 }, // Left
+};
+const getNextCoords = ({ row, col }, direction) => ({
+  row: row + directions[direction].row,
+  col: col + directions[direction].col,
+});
+const getGridCell = ({ row, col }) => grid[row] && grid[row][col];
+const swapCells = ({ row: rowA, col: colA }, { row: rowB, col: colB }) => {
+  [grid[rowA][colA], grid[rowB][colB]] = [grid[rowB][colB], grid[rowA][colA]];
+};
+const findCharCoords = (char) =>
+  grid.flatMap((line, rowNum) =>
+    line.reduce(
+      (matches, cell, colNum) =>
+        cell === char ? matches.concat({ row: rowNum, col: colNum }) : matches,
+      [],
+    ),
+  );
+
+// Test recursively whether this line can be pushed in the direction specified
+const checkPushPossible = (coords, direction) => {
+  const nextCoords = getNextCoords(coords, direction);
+  const nextCell = getGridCell(nextCoords);
+  if (nextCell === "#") {
+    return false;
+  }
+  if (nextCell === ".") {
+    return true;
+  }
+  return checkPushPossible(nextCoords, direction);
+};
+
+// Perform the push itself as a series of swaps recursively
+const push = (coords, direction) => {
+  const nextCoords = getNextCoords(coords, direction);
+  const nextCell = getGridCell(nextCoords);
+  if (nextCell === ".") {
+    // Base case to end the sequence of swaps
+    swapCells(coords, nextCoords);
+    return;
+  }
+  // Check whether the push is possible before attempting anything
+  if (!checkPushPossible(coords, direction)) {
+    return;
+  }
+  // Move the rest of the line
+  push(nextCoords, direction);
+  // Move the current cell into the gap left by pushing the rest of the line
+  swapCells(coords, nextCoords);
+};
+
+// Calculate score as described in the problem
+const getBoxScore = ({ row, col }) => row * 100 + col;
+
+// Have the robot perform all the moves
+moves.forEach((move) => {
+  const robotCoords = findCharCoords("@")[0];
+  push(robotCoords, move);
+});
+
+// Get the total score
+const score = findCharCoords("O")
+  .map(getBoxScore)
+  .reduce((total, next) => total + next, 0);
+console.log(score);
