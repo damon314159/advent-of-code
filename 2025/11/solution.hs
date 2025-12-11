12ba0acdd1c9898a6d180753d05e9c37b668dd53
Damon <126731021+damon314159@users.noreply.github.com>
2025-12-11 22:51:41 +0000

    Add day 11 solutions

--- 2025/11/solution.hs
+++ 2025/11/solution.hs
@@ -1,26 +1,18 @@
-import Data.Map (Map)
-import Data.MemoTrie
-import GHC.Generics (Generic)
+import Data.MemoTrie (memo2)
 import System.Environment (getArgs)
 
-newtype Node = Node String deriving (Generic, Eq, Ord, Show)
+type Node = String
 
 type Graph = [(Node, [Node])] -- assoc list / adjacency list
 
 type Solution = Int
-
-instance HasTrie Node where
-  newtype Node :->: b = NodeTrie {unNodeTrie :: Reg Node :->: b}
-  trie = trieGeneric NodeTrie
-  untrie = untrieGeneric unNodeTrie
-  enumerate = enumerateGeneric unNodeTrie
 
 parser :: String -> Graph
 parser = map getAdjacency . lines
   where
     getAdjacency line =
       let (label, others) = break (== ':') line
-       in (Node label, map Node . words . tail $ others)
+       in (label, words . tail $ others)
 
 countWays :: Node -> Node -> Graph -> Int
 countWays start target graph = goMemo start target
@@ -33,18 +25,18 @@ countWays start target graph = goMemo start target
           Nothing -> 0 -- handle dead end
 
 solve1 :: Graph -> Solution
-solve1 = countWays (Node "you") (Node "out")
+solve1 = countWays "you" "out"
 
 solve2 :: Graph -> Solution
 solve2 graph = svrToFft * fftToDac * dacToOut + svrToDac * dacToFft * fftToOut
   where
-    svrToFft = countWays (Node "svr") (Node "fft") graph
-    fftToDac = countWays (Node "fft") (Node "dac") graph
-    dacToOut = countWays (Node "dac") (Node "out") graph
+    svrToFft = countWays "svr" "fft" graph
+    fftToDac = countWays "fft" "dac" graph
+    dacToOut = countWays "dac" "out" graph
     -- or:
-    svrToDac = countWays (Node "svr") (Node "dac") graph
-    dacToFft = countWays (Node "dac") (Node "fft") graph
-    fftToOut = countWays (Node "fft") (Node "out") graph
+    svrToDac = countWays "svr" "dac" graph
+    dacToFft = countWays "dac" "fft" graph
+    fftToOut = countWays "fft" "out" graph
 
 main :: IO ()
 main = do
